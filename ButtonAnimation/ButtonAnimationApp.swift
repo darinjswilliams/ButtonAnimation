3cfc29fc77ac8d755b32d8fee8e61c423bb5283a
darinwilliams <darinjswilliams@icloud.com>
2024-09-13 11:04:53 -0500

    Initial Commit

---
+++ ButtonAnimation/ButtonAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAnimationApp.swift
+//  ButtonAnimation
+//
+//  Created by Darin Williams on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
