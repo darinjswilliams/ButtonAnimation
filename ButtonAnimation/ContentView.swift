b23151edd009ed2271151d0afa37e77c3c1b390d
darinwilliams <darinjswilliams@icloud.com>
2024-09-13 11:33:33 -0500

    First Commit

--- ButtonAnimation/ContentView.swift
+++ ButtonAnimation/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPressed: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Tap Me")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
+                    .scaleEffect(isPressed ? 1.5 : 1.0)
+                    .animation(.easeOut(duration: 0.2), value:  isPressed)
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged {_ in isPressed = true
+                            }
+                            .onEnded {
+                                _ in isPressed = false
+                            }
+                    )
+                }
         }
-        .padding()
     }
-}
 
 #Preview {
     ContentView()
